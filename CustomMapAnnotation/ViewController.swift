d231b5f0f8359ae4fffbd4824faafb4bade686e7
Luke Van In <lukevanin@gmail.com>
2016-07-14 21:31:33 +0200

    Initial Commit

---
+++ CustomMapAnnotation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomMapAnnotation
+//
+//  Created by Luke Van In on 2016/07/14.
+//  Copyright © 2016 Luke Van In. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
